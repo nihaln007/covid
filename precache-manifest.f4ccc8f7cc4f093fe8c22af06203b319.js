0816839b553d0af34586f296f912be3e4712ec3d
Nihal <nihalnyt@gmail.com>
2020-09-15 11:09:19 +0530

    Updates

---
+++ precache-manifest.f4ccc8f7cc4f093fe8c22af06203b319.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "394b6c5253ab4f321e72100ebd797cc9",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "10fd02b5475e4bbdfc48",
+    "url": "/covid/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "89a88145c8074ea7ae67",
+    "url": "/covid/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "10fd02b5475e4bbdfc48",
+    "url": "/covid/static/js/2.90adf312.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid/static/js/2.90adf312.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89a88145c8074ea7ae67",
+    "url": "/covid/static/js/main.fc88b85a.chunk.js"
+  },
+  {
+    "revision": "2166c9592012ff450931",
+    "url": "/covid/static/js/runtime-main.f3ff14b1.js"
+  }
+]);
